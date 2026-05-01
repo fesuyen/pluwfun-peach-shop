4abf83523ec0471da90f608e21176d46a7fb81fa
pluwfun <chowearyean@gmail.com>
2026-05-01 17:25:29 +0800

    Refactor App component structure and imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home'; // 將原本的首頁內容移到這裡
-import AdminPage from './pages/AdminPage'; // 後台管理頁面
+import Home from './pages/Home';
+import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          {/* 前台首頁 */}
-          <Route path="/" element={<Home />} />
-          {/* 後台管理：確保這個路徑能進去 */}
-          <Route path="/admin" element={<AdminPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </div>
   );
 }
 
