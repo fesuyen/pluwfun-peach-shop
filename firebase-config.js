17e00219c91807dd6575cf06d8195fd872ccdfa2
pluwfun <chowearyean@gmail.com>
2026-05-02 17:41:39 +0800

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDpWEo4GEq2Zu65DoMeGt1pehqxw2V2ong",
-  authDomain: "pluwfun-peach-shop.firebaseapp.com",
-  projectId: "pluwfun-peach-shop",
-  storageBucket: "pluwfun-peach-shop.firebasestorage.app",
-  messagingSenderId: "661743381662",
-  appId: "1:661743381662:web:3c86ca5c0b52d91cf69f8f",
-  measurementId: "G-MSYSCWSBVF",
-};
